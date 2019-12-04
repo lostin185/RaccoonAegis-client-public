d8c192cf9859cc62d9213dc30265722ab77ccba4
Sanghoon Kim <lostin185@gmail.com>
2019-12-04 20:57:27 +0900

    리팩토링 - declarative content 제거
    제거해도 extension이 잘 작동합니다.
    그래서 제거합니다.

--- background.js
+++ background.js
@@ -1,18 +1,3 @@
-const kMatchRule = {
-  conditions: [
-    new chrome.declarativeContent.PageStateMatcher({
-      pageUrl: { hostEquals: "<all_urls>", schemes: ['http', 'https'] }
-    })
-  ],
-  actions: [new chrome.declarativeContent.ShowPageAction()]
-};
-
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    chrome.declarativeContent.onPageChanged.addRules([kMatchRule]);
-  });
-});
-
 chrome.storage.local.set({
   status: {
     signinPage: true,
